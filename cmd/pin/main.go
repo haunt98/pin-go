6c0f62022dc87cbfe8c3c25f1c685556cd3592b7
Hau Nguyen <hauvipapro@gmail.com>
2022-04-02 00:35:11 +0700

    feat: init with sqlite3

---
+++ cmd/pin/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const dataFilename = "data.sqlite3"
+
+func main() {
+	db, err := sql.Open("sqlite3", getDataFilePath())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
+
+	// app, err := cli.NewApp(db)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// app.Run()
+}
+
+// Should be ./data.sqlite3
+func getDataFilePath() string {
+	return filepath.Join(".", dataFilename)
+}
